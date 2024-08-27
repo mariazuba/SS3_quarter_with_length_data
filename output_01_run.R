23333f5beff0b14f6abb8f95ac4032231b658956
mariazuba <mariazuba@gmail.com>
2024-08-27 09:38:01 +0200

    Actualizados output/run/S0 2024-08-27 09:38:01

--- output_01_run.R
+++ output_01_run.R
@@ -26,15 +26,23 @@ esc<-list.files(run_esc)
 for(i in 1:length(esc)){
   run.dir  <- paste0(run_esc,esc[i])
   output <- r4ss::SS_output(dir = run.dir,forecast=FALSE)
+  summary <- read.table(paste0(run.dir,"/ss_summary.sso"),header=F,sep="",na="NA",fill=T)
+  # setting 
+  R0 <- output$estimated_non_dev_parameters["SR_LN(R0)", "Value"]
   
   mkdir(paste0("output/run/",esc[i]))
   run_out<<-paste0("output/run/",esc[i])
+  
+  # Definir la ruta al directorio que quieres borrar
+  dir_to_remove <- paste0("output/run/",esc[i],"/plots")
+  # Ejecutar el comando para borrar la carpeta
+  system(paste("rm -r", shQuote(dir_to_remove)))
   
   r4ss::SS_plots(replist = output, dir = run_out,
                  printfolder = "plots")
   
   # Write .RData ----
-  save(output,           
+  save(output,  summary, R0,             
        file=paste0(run_out,"/output.RData"))
 }
 
